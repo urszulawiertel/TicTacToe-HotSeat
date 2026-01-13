20fe5ec4d2a10ea151ae3bd1b9bafa27b582af6b
urszulawiertel <urszulawiertel@gmail.com>
2026-01-13 18:09:04 +0100

    Initial SwiftUI project

---
+++ SwiftUIPlayground/SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Ula on 13/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
